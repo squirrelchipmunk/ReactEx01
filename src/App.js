919c078f0df3306b9b0374c9838b69130872f774
squirrel <ggoong8@gmail.com>
2022-06-29 23:16:20 +0900

    useRef

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useMemo, useState } from 'react';
+import { createRef, useMemo, useRef, useState } from 'react';
 
 // 0. React 엔진 : 데이터 변경 감지해서 ui 그리기
 // 1. 실행 -> index.html : (Single Page Application, SPA)
@@ -16,47 +16,33 @@ import { useMemo, useState } from 'react';
 //    - 라이브러리(부트스트랩, componenet-styled)
 
 function App() {
-  //useMemo -> 메모라이제이션(기억)
+  //useRef(디자인)
+  // dom 변경 기능
 
-  const [list, setList] = useState([1, 2, 3, 4]);
-  const [str, setStr] = useState('합계');
+  const myRef = useRef(null); // 1개만 적용됨
 
-  const getAddResult = () => {
-    let sum = 0;
-    list.forEach((i) => (sum = sum + i));
-    console.log('sum 실행 됨 ', sum);
-    return sum;
-  };
+  const [list, setList] = useState([
+    { id: 1, name: '길동' },
+    { id: 2, name: '꺽정' },
+  ]);
 
-  // 문자 변경하기 버튼 클릭 시 getAddResult 막는 방법
-  const addResult = useMemo(() => getAddResult(), [list]);
-  // getAddResult()는 list가 변경될 때만 다시 실행
+  const myRefs = Array.from({ length: list.length }).map(() => createRef()); // 배열로 useRef 만들기
 
   return (
     <div>
       <button
         onClick={() => {
-          setStr('안녕');
+          myRef.current.style.backgroundColor = 'red';
+          myRefs[0].current.style.backgroundColor = 'red';
+          myRefs[1].current.style.backgroundColor = 'red';
         }}
       >
-        문자 변경하기
+        색 변경
       </button>
-
-      <button
-        onClick={() => {
-          setList([...list, 5]);
-        }}
-      >
-        리스트에 값 추가하기
-      </button>
-      <div>
-        {list.map((i) => (
-          <h1>{i}</h1>
-        ))}
-      </div>
-      <div>
-        {str} : {addResult}
-      </div>
+      <div ref={myRef}> 박스 </div>
+      {list.map((user, index) => (
+        <h1 ref={myRefs[index]}>{user.name}</h1>
+      ))}
     </div>
   );
 }
