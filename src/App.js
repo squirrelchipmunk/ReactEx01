e9be2667de7be20ba0112efbb183a4814a4ed557
squirrel <ggoong8@gmail.com>
2022-07-02 00:47:06 +0900

    list write 1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './pages/HomePage';
-import Header from './components/Header';
-import Footer from './components/Footer';
+// 글쓰기, 글삭제, 글목록
+
 import { Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/Navigation';
+import ListPage from './pages/ListPage';
+import WritePage from './pages/WritePage';
 
-// 0. React 엔진 : 데이터 변경 감지해서 ui 그리기
-// 1. 실행 -> index.html : (Single Page Application, SPA)
-// 2. JSX 문법 : 자바스크립트 코드에 html 문법 사용
-
-// (1). 1개의 dom만 리턴가능
-// (2). 변수 선언은 let 혹은 const 하기
-// (3). if X, 3항 연산자 O
-// (4). 조건부 렌더링 (조건 && 값)
-// (5). css 디자인
-//    - 내부 (비추)
-//    - 외부 파일
-//    - 라이브러리(부트스트랩, componenet-styled)
-
-// 요청 주소에 따라 Route 부분의 객체만 변경됨
 function App() {
   return (
-    <div>
-      <Header />
+    <>
+      <Navigation />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login/:id" element={<LoginPage />} />
+        <Route path="/" element={<ListPage />}></Route>
+        <Route path="/write" element={<WritePage />}></Route>
       </Routes>
-      <Footer />
-    </div>
+    </>
   );
 }
 
